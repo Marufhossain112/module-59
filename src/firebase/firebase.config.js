b3b477a6a01ab64266c47783851ae366e4a66e5d
maruf <dev.maruf112@gmail.com>
2022-10-18 23:19:49 +0600

    1/ login and register button added to the navbar and some setup done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKyC-kdeZ0mgjlJJ4SdIEAPUas1K2twv8",
+  authDomain: "ema-john-simple-46906.firebaseapp.com",
+  projectId: "ema-john-simple-46906",
+  storageBucket: "ema-john-simple-46906.appspot.com",
+  messagingSenderId: "424996236503",
+  appId: "1:424996236503:web:5b23037a04808d90259492",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
