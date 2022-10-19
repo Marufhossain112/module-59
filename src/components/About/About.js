50a13a4dcc63d8842521ba670e4d782412f9ebbd
maruf <dev.maruf112@gmail.com>
2022-10-19 07:52:11 +0600

    4/ get and display form data

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { AuthContext } from "../../context/UserContext";
 
 const About = () => {
-    return (
-        <div>
-            <h2>Secret about us!!!</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Secret about us!!! </h2>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,60 +1,83 @@
-import React, { useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import "./Register.css";
+import { Link } from "react-router-dom";
 // import { AuthContext } from '../../contexts/UserContext';
 
 const Register = () => {
-    const [error, setError] = useState(null);
-    // const { createUser } = useContext(AuthContext);
+  //   const [error, setError] = useState(null);
+  // const { createUser } = useContext(AuthContext);
+  // const handleSubmit = (event) => {
+  //     event.preventDefault();
+  //     const form = event.target;
+  //     const email = form.email.value;
+  //     const password = form.password.value;
+  //     const confirm = form.confirm.value;
 
-    // const handleSubmit = (event) => {
-    //     event.preventDefault();
-    //     const form = event.target;
-    //     const email = form.email.value;
-    //     const password = form.password.value;
-    //     const confirm = form.confirm.value;
+  //     if (password.length < 6) {
+  //         setError('Password should be 6 characters or more.');
+  //         return;
+  //     }
 
-    //     if (password.length < 6) {
-    //         setError('Password should be 6 characters or more.');
-    //         return;
-    //     }
+  //     if (password !== confirm) {
+  //         setError('Your Password did not match');
+  //         return;
+  //     }
 
-    //     if (password !== confirm) {
-    //         setError('Your Password did not match');
-    //         return;
-    //     }
+  //     createUser(email, password)
+  //         .then(result => {
+  //             const user = result.user;
+  //             console.log(user);
+  //             form.reset();
+  //         })
+  //         .catch(error => console.error(error));
 
-    //     createUser(email, password)
-    //         .then(result => {
-    //             const user = result.user;
-    //             console.log(user);
-    //             form.reset();
-    //         })
-    //         .catch(error => console.error(error));
+  // }
 
-    // }
+  const [error, setError] = useState(null);
 
-    return (
-        <div className='form-container'>
-            <h2 className='form-title'>Sign Up</h2>
-            <form /* onSubmit={handleSubmit} */>
-                <div className="form-control">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="email" required />
-                </div>
-                <div className="form-control">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="password" required />
-                </div>
-                <div className="form-control">
-                    <label htmlFor="confirm">Confirm Password</label>
-                    <input type="password" name="confirm" required />
-                </div>
-                <input className='btn-submit' type="submit" value="Sign Up" />
-            </form>
-            <p>Already Have an Account <Link to='/login'>Login</Link></p>
-            <p className='text-error'>{error}</p>
+  const handleForm = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const confirm = form.confirm.value;
+    // console.log(email, password, confirm);
+
+    // handle password error
+    if (password.length < 6) {
+      setError("Password should be at least 6 characters or more..");
+      return;
+    }
+    if (password !== confirm) {
+      setError("Password does not match");
+      return;
+    }
+  };
+
+  return (
+    <div className="form-container">
+      <h2 className="form-title">Sign Up</h2>
+      <form onSubmit={handleForm}>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" required />
         </div>
-    );
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="confirm">Confirm Password</label>
+          <input type="password" name="confirm" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Sign Up" />
+      </form>
+      <p>
+        Already Have an Account <Link to="/login">Login</Link>
+      </p>
+      <p className="text-error">{error}</p>
+    </div>
+  );
 };
 
 export default Register;
