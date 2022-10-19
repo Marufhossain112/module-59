e5a4fabc117ec74bd635afdca11a477569099194
maruf <dev.maruf112@gmail.com>
2022-10-19 12:23:14 +0600

    Get user successfully

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,11 +1,20 @@
-import React, { useContext, useState } from "react";
+import React, {
+  useContext,
+  useState
+} from "react";
 import "./Register.css";
-import { Link } from "react-router-dom";
-// import { AuthContext } from '../../contexts/UserContext';
+import {
+  Link
+} from "react-router-dom";
+import {
+  AuthContext
+} from '../../context/UserContext';
 
 const Register = () => {
-  //   const [error, setError] = useState(null);
-  // const { createUser } = useContext(AuthContext);
+  // const [error, setError] = useState(null);
+  const {
+    createUser
+  } = useContext(AuthContext);
   // const handleSubmit = (event) => {
   //     event.preventDefault();
   //     const form = event.target;
@@ -33,7 +42,8 @@ const Register = () => {
 
   // }
 
-  const [error, setError] = useState(null);
+  const [error,
+    setError] = useState(null);
 
   const handleForm = (e) => {
     e.preventDefault();
@@ -42,6 +52,14 @@ const Register = () => {
     const password = form.password.value;
     const confirm = form.confirm.value;
     // console.log(email, password, confirm);
+
+    createUser(email, password)
+    .then(result=> {
+      const user = result.user;
+      console.log(user);
+    }).catch(error=> {
+      console.error(error)
+    })
 
     // handle password error
     if (password.length < 6) {
@@ -61,23 +79,25 @@ const Register = () => {
         <div className="form-control">
           <label htmlFor="email">Email</label>
           <input type="email" name="email" required />
-        </div>
+    </div>
         <div className="form-control">
           <label htmlFor="password">Password</label>
           <input type="password" name="password" required />
-        </div>
+    </div>
         <div className="form-control">
           <label htmlFor="confirm">Confirm Password</label>
           <input type="password" name="confirm" required />
-        </div>
+  </div>
         <input className="btn-submit" type="submit" value="Sign Up" />
-      </form>
+</form>
       <p>
         Already Have an Account <Link to="/login">Login</Link>
-      </p>
-      <p className="text-error">{error}</p>
-    </div>
-  );
+</p>
+      <p className="text-error">
+{error}
+</p>
+</div>
+);
 };
 
 export default Register;
--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,13 +1,26 @@
-import React, { createContext, useState } from "react";
-import { getAuth } from "firebase/auth";
+import React, {
+  createContext,
+  useState
+} from "react";
+import {
+  getAuth,
+  createUserWithEmailAndPassword
+} from "firebase/auth";
 import app from "../firebase/firebase.config";
 const auth = getAuth(app);
 
 export const AuthContext = createContext();
-const UserContext = ({ children }) => {
-  const [user, setUser] = useState(null);
-
-  const authInfo = { user };
+const UserContext = ({
+  children
+}) => {
+  const [user,
+    setUser] = useState(null);
+  const createUser = (email, password)=> {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+  const authInfo = {
+    user,createUser
+  };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
